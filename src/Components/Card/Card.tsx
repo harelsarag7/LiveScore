9ded464b9668e5da2f68c322d0e355fffe81d92e
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2022-11-07 07:44:33 +0200

    added function get scorers

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -109,6 +109,7 @@ import ReactDOM from "react-dom";
 const apiKey = "581fb55ff92d00056049811d83a45652c46ae3ff89ec0166c24366d92d25a22c"
 function Card(): JSX.Element {
 const [teams, setTeams] = useState<liveScore>({});
+const counter = 2;
 
     // const apiKey = "5aa9fcce24396558941310cd46d3a4499e45318738e277271468dd16c1e5e412"
 //     let homeTeamImage ;
@@ -130,9 +131,9 @@ const [teams, setTeams] = useState<liveScore>({});
 
 
 useEffect(() => {
-  getApiData().then(res => {
+  getApiDataLastMonth().then(res => {
     setTeams(res)
-    // console.log(teams);
+    console.log(res.result);
   });
   
 }, []);
@@ -146,45 +147,145 @@ useEffect(() => {
 //   setUsers(response);
 // };
 
-async function getApiData() {
+
+
+// goalsscorers - need to change to modal or collape
+function getScores(game: any) {
+  let home_scorers = []
+  let away_scorers = []
+  for(let i = 0; i < game.goalscorers.length; i++){
+    if(game.goalscorers[i].home_scorer){
+      home_scorers.push(game.goalscorers[i].home_scorer? game.goalscorers[i].home_scorer : <></> )
+    } else if(game.goalscorers[i].away_scorers){
+
+      away_scorers.push(game.goalscorers[i].away_scorers? game.goalscorers[i].away_scorers : <></> )
+    } 
+
+    if(home_scorers.length === 0){
+      home_scorers.push("No Goals")
+    } else if(away_scorers.length === 0){
+      away_scorers.push("No Goals")
+    } 
+  }
+
+  // alert(`Home: ` + home_scorers `\n` + "Away: " + away_scorers )
+  alert(`${game.event_home_team}: ${home_scorers} \n  ${game.event_away_team}: ${away_scorers} `)
+  
+//  return console.log(parameter)
+}
+
+
+
+
+  // regular api
+// async function getApiData() {
+//   const response:liveScore = await fetch(
+//     `https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=${apiKey}`
+//   ).then((response) => response.json());
+
+
+// async function getApiData() {
+//   const response:liveScore = await fetch(
+//     `https://apiv2.allsportsapi.com/football/?met=Livescore&countryId=6&APIkey=${apiKey}`
+//   ).then((response) => response.json());
+
+
+
+
+//country id 62 = israel
+//country id 6 = spain
+
+
+async function getApiDataLastMonth() {
   const response:liveScore = await fetch(
-    `https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=${apiKey}`
+    `https://apiv2.allsportsapi.com/football/?met=Fixtures&countryId=62&APIkey=${apiKey}&from=2022-10-01&to=2022-11-06`
   ).then((response) => response.json());
-  // .then(response => console.log(response.result);
-  // )
-  // console.log(response.result[0]);
+
+
+
   
     return response;
 }
 
 
-
+let teamsMap = teams.result;
 // return (
-return ( 
-    <div className="Card">
-      {/* <p>{teams.map((team) => {team.event_key})}</p> */}
-      <div className="home-team-image team">
-        
-          {teams.result? teams.result[0].event_home_team : "Loading"}
-          <img className="team-logo" src={teams.result? teams.result[0].home_team_logo : "Loading"} alt="" />
-        </div>
-       VS
-      <div className="home-team-image team">{teams.result? teams.result[0].event_away_team : "Loading"}
-          <img className="team-logo" src={teams.result? teams.result[0].away_team_logo : "Loading"} alt="" />
+  return ( teams.result? 
+      // {teamsMap.map((team) => console.log(team))}
+      <div className="Card">
+    {/* <p>{teams.map((team) => {team.event_key})}</p> */}
+    <div className="info-container">
+
+    <div className="home-team-image team">
+      <ul>
+      {teamsMap? teamsMap.map((team) =>
+       <li>{team.country_name}</li>): <></>}
+      </ul>
+        {teams.result[counter].event_home_team}
+        <img className="team-logo" src={teams.result[counter].home_team_logo} alt="" />
       </div>
-       {/* {users.result[0].event_key ?  
-       <li key={users.result[0].country_name}>{users.result[0].country_name}</li> : <></>
-        }
-      </ul> */}
+      <div className="score-container">
+         VS
+         <span className="score-live" >{teams.result[counter].event_final_result}</span>
+         <span>{teams.result[counter].event_status}'</span>
+      </div>
+    <div className="home-team-image team">{teams.result[counter].event_away_team}
+        <img className="team-logo" src={teams.result[counter].away_team_logo} alt="" />
     </div>
+  </div>
 
+    {/* <button onClick={getScores(teams.result[counter].goalscorers)}>click */}
+    <div className="statistics-container">
+    <button onClick={() => getScores(teams.result? teams.result[counter] : "No Goals")}>Goals</button>
+    </div>
+    {/* <button onClick={() => {teams.result? teams.result[counter].goalscorers.length: <></>}}>click</button> */}
 
-    // <div className="Card">
-    //     <div className="home-team-image">Israel</div>
-    //     VS
-    //     <div className="home-team-image">{}</div>
-    // </div>
-);
+     {/* {users.result[counter].event_key ?  
+     <li key={users.result[counter].country_name}>{users.result[counter].country_name}</li> : <></>
+      }
+    </ul> */}
+  </div>
+    : <></>)
+// return (teams.result?
+
+//     <div className="Card">
+//       {/* <p>{teams.map((team) => {team.event_key})}</p> */}
+//       <div className="info-container">
+
+//       <div className="home-team-image team">
+        
+//           {teams.result[counter].event_home_team}
+//           <img className="team-logo" src={teams.result[counter].home_team_logo} alt="" />
+//         </div>
+//         <div className="score-container">
+//            VS
+//            <span className="score-live" >{teams.result[counter].event_final_result}</span>
+//            <span>{teams.result[counter].event_status}'</span>
+//         </div>
+//       <div className="home-team-image team">{teams.result[counter].event_away_team}
+//           <img className="team-logo" src={teams.result[counter].away_team_logo} alt="" />
+//       </div>
+//     </div>
+
+//       {/* <button onClick={getScores(teams.result[counter].goalscorers)}>click */}
+//       <div className="statistics-container">
+//       <button onClick={() => getScores(teams.result? teams.result[counter] : "No Goals")}>Goals</button>
+//       </div>
+//       {/* <button onClick={() => {teams.result? teams.result[counter].goalscorers.length: <></>}}>click</button> */}
+
+//        {/* {users.result[counter].event_key ?  
+//        <li key={users.result[counter].country_name}>{users.result[counter].country_name}</li> : <></>
+//         }
+//       </ul> */}
+//     </div> : <></>
+//     // }
+
+//     // <div className="Card">
+//     //     <div className="home-team-image">Israel</div>
+//     //     VS
+//     //     <div className="home-team-image">{}</div>
+//     // </div>
+// );
     
 };
 
